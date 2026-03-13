3ddba3533c10006c632180e313dfccd830589667
hongtay <xanmangnga872@gmail.com>
2026-03-13 14:30:16 +0700

    exercise: Objects

--- ex-2.js
+++ ex-2.js
@@ -8,3 +8,11 @@ const product = {
 };
 let socialMediaShare = "facebookShare";
 // Start code here
+product.ratings = 4.5 ;
+product.code = "IT0101010" ;
+product.photo ="https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+product.price = 6000
+delete product.ratings ;
+product[socialMediaShare] = 'socialMediaShare';
+
+console.log(product)
